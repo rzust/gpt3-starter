53a9d068eb80af864d70ec66a00b8cb18b7e1264
rzust <renato.zust@dlabs.si>
2022-12-20 15:50:30 -0500

    fix openai request on edge function

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,5 +1,3 @@
-import { Configuration, OpenAIApi } from 'openai';
-
 export const config = {
     runtime: 'experimental-edge',
 }
@@ -8,23 +6,42 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 })
 
-const openai = new OpenAIApi(configuration);
 const basePromptPrefix = "Give me the key points, lessons and action points of the following book. Please separate each section and explain each point with examples in a separate sentence  in the style of naval ravikan. Don't limit yourself in the amount of points and lessons given: ";
 
-export default async function (req) {
-    console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
-
-    const baseCompletion = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${basePromptPrefix}${req.body.userInput}`,
-        temperature: 0.7,
+async function generateCompletions(prompt, model) {
+    const data = {
+        model,
+        prompt,
         max_tokens: 250,
-    });
+    };
 
-    const basePromptOutput = baseCompletion.data.choices.pop();
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${configuration.apiKey}`
+        },
+        body: JSON.stringify(data)
+    };
+
+    const response = await fetch('https://api.openai.com/v1/completions', options);
+
+    if (response.ok) {
+        const data = await response.json();
+        return data.choices.pop();
+    } else {
+        throw new Error(response.statusText);
+    }
+}
+
+export default async function (req) {
+
+    const { searchParams } = new URL(req.url)
+    const prompt = searchParams.get('prompt')
+    const completions = await generateCompletions(`${basePromptPrefix}${prompt}`, "text-davinci-003");
 
     return new Response(
-        JSON.stringify({ output: basePromptOutput }),
+        JSON.stringify({ output: completions }),
         {
             status: 200,
             headers: {
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
     setIsGenerating(true);
 
     console.log("Calling OpenAI...");
-    const response = await fetch('/api/generate', {
+    const response = await fetch('/api/generate?prompt=adfs', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
